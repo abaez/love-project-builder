180251d456c1be1db4c61311206722fa4940d64c
Alejandro Baez <Alejan.Baez@gmail.com>
2015-01-14 15:32:45 -0500

    made append_files

--- love_init.lua
+++ love_init.lua
@@ -1,14 +1,15 @@
 #!/usr/bin/lua
 
-if not io.open("~/.love_project.lua") then
-  print("making ~/.config file")
-  io.open(".config", "w"):write('return {\n\tlpb = ""\n}')
+if not io.open("~/.love_init.ld") then
+  print("making ~/.love_init.ld file")
+  io.open("~/.love_init.ld", "w")
+  :write('return {\n\tsrc = ""\n}')
 
   print("Please change string for ~/.love_project.lua")
   os.exit()
 end
 
-local conf = require(".love_project.lua")
+local user = require(".love_init.ld")
 
 
 --- writes a line to the file selected.
@@ -21,36 +22,41 @@ function write_line(loc, file, str, m)
   io.open(loc .. "/" .. file, m or "a+"):write(str):close()
 end
 
---- checks love_init for new updates.
--- @function has_update
-function has_update()
-  for line in io.popen("cd  " .. conf.lpb .. "; hg incoming"):lines() do
-    if line:match("no changes found") then
-      return false
-    end
-  end
-    return true
-end
-
 --- creates the intialized directory for the love project.
 -- @function build_env
 -- @param loc location of the love project path.
--- @param template the mercurial repository to use as a project template.
-function build_env(loc, template)
-  local template = template or conf.lpb
-  write_line(loc, "config.ld", string.format("project = %q", arg[1]))
-  os.execute("rm -rf " .. loc .. "/.hg")
+-- @param name the name love project path.
+-- @param src the source path of the src.
+function build_env(loc, name, src)
+  local src  = src or user.src
+
   os.execute("cd " .. loc ..
              "; hg init; hg commit -A -m 'initial commit'")
 end
 
+
+--- makes the files from template.
+-- @param loc location of the project directory.
+-- @param name the name of project.
+-- @param user a @{user} table.
+local function make_files(loc, name, user)
+  write_line(loc, "config.ld", string.format(
+    "project = %q\ntitle = %q", name, name .. " docs"))
+  write_line(loc, "conf.lua", string.format(
+    "t.title  = %q\n  t.author = %q\n  t.url = %q\nend",
+    name, user.author, user.url))
+  write_line(loc, "main.lua", "--- " .. name, "r+")
+end
+
+
 local help = [=[
   love_init v0.1
-  usage: love_init <name> [-p <path>] [-d <path>]
+  usage: love_init <name> [-s <source>] [-p <path>] [-d <path>]
 
   Available actions:
   <name>      name of the lapis project
   -p <path>   the location of the installation for the project.
+  -s <source> the location for the source of love_init.
   -h          prints this text
 ]=]
 
@@ -61,8 +67,12 @@ else
     for i = 2, #arg do
       if arg[i] == '-p' then
         print("making project:", arg[i+1])
-        build_env(arg[i+1] .. "/" .. arg[1])
+        build_env(arg[i+1] .. "/" .. arg[1], arg[1])
       end
     end
+  else
+    assert(arg[1] ~= '-d' and arg[1] ~= '-p' and arg[1] ~= '-s', help)
+    print("making environment project: " .. arg[1])
+    build_env(io.popen("pwd"):read() .. "/" .. arg[1], arg[1])
   end
 end
